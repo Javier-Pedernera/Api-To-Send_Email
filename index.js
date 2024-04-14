d74ba7d3432dbe35b2b6be41b821bbec9bd00779
JaviPeder~ <javierpedernera@gmail.com>
2024-04-14 12:04:02 -0300

    cambios en post

--- index.js
+++ index.js
@@ -18,40 +18,40 @@ const emailTemplate = fs.readFileSync(templatePath, 'utf8');
 
 app.get("/", (req, res) => { res.send("Express on Vercel"); });
 
-// app.post('/send-email', async (req, res) => {
-//   const { nombre, apellidos, empresa, productoServicio, email, movil, pais, descripcion, consentimiento, empleados } = req.body;
+app.post('/send-email', async (req, res) => {
+  const { nombre, apellidos, empresa, productoServicio, email, movil, pais, descripcion, consentimiento, empleados } = req.body;
 
-//   let html = emailTemplate
-//     .replace('{{nombre}}', nombre)
-//     .replace('{{apellidos}}', apellidos)
-//     .replace('{{empresa}}', empresa)
-//     .replace('{{productoServicio}}', productoServicio)
-//     .replace('{{email}}', email)
-//     .replace('{{movil}}', movil)
-//     .replace('{{pais}}', pais)
-//     .replace('{{descripcion}}', descripcion)
-//     .replace('{{consentimiento}}', consentimiento ? 'Sí' : 'No')
-//     .replace('{{empleados}}', empleados);
+  let html = emailTemplate
+    .replace('{{nombre}}', nombre)
+    .replace('{{apellidos}}', apellidos)
+    .replace('{{empresa}}', empresa)
+    .replace('{{productoServicio}}', productoServicio)
+    .replace('{{email}}', email)
+    .replace('{{movil}}', movil)
+    .replace('{{pais}}', pais)
+    .replace('{{descripcion}}', descripcion)
+    .replace('{{consentimiento}}', consentimiento ? 'Sí' : 'No')
+    .replace('{{empleados}}', empleados);
 
-//   try {
-//     const { data, error } = await resend.emails.send({
-//         from: 'Cliente Gescotec <jpedernera@gescotec.cl>',
-//         to: ['contactanos@gescotec.cl'],
-//         subject: 'Formulario de contacto- Gescotec',
-//         html: html,
-//         replyTo: email
-//       });
-//     if (error) {
-//       console.log(error);
-//       res.status(400).json({ error: 'Error al enviar el correo electrónico' });
-//     } else {
-//       res.status(200).json({ message: 'Correo electrónico enviado correctamente' });
-//     }
-//   } catch (err) {
-//     console.error('Error al enviar el correo electrónico:', err);
-//     res.status(500).json({ error: 'Error interno del servidor' });
-//   }
-// });
+  try {
+    const { data, error } = await resend.emails.send({
+        from: 'Cliente Gescotec <jpedernera@gescotec.cl>',
+        to: ['contactanos@gescotec.cl'],
+        subject: 'Formulario de contacto- Gescotec',
+        html: html,
+        replyTo: email
+      });
+    if (error) {
+      console.log(error);
+      res.status(400).json({ error: 'Error al enviar el correo electrónico' });
+    } else {
+      res.status(200).json({ message: 'Correo electrónico enviado correctamente' });
+    }
+  } catch (err) {
+    console.error('Error al enviar el correo electrónico:', err);
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
